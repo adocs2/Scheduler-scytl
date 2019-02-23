3a59e76a6efeb8a8d436c24a4ffa0da0bd127189
Afonso Dias <afonso-dias2011@hotmail.com>
2019-02-23 01:31:43 -0300

    style: changing app name to 'Scheduler Scytl'

--- quasar.conf.js
+++ quasar.conf.js
@@ -87,7 +87,7 @@ module.exports = function (ctx) {
       // workboxPluginMode: 'InjectManifest',
       // workboxOptions: {},
       manifest: {
-        // name: 'Quasar App',
+        // name: 'Scheduler Scytl',
         // short_name: 'Quasar-PWA',
         // description: 'Best PWA App in town!',
         display: 'standalone',
